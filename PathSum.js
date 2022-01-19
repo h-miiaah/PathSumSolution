168e3757d2d6ca7f7705844bd95d3f0d82268c01
h-miiaah <haim.miiaah5@gmail.com>
2022-01-18 23:12:40 -0500

    final retrun

--- PathSum.js
+++ PathSum.js
@@ -14,4 +14,5 @@
  var hasPathSum = function(root, targetSum) {
     if (!root) return false;
     if (root.val === targetSum && !root.left && !root.right) return true;
+    return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val)
 };
